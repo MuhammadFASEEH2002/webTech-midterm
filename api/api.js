0423afa3aa0da79e45ed458f5eb7924fe76a286a
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2023-11-06 00:49:59 +0500

    deleted extra files

--- api/api.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-const instance = axios.create({
-    baseURL: "http://localhost:9207",
-});
-
-module.exports = instance;
--- models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { model, Schema } = require('mongoose');
-
-const orderSchema = new Schema({
-    RowId: Number,
-    qty: Number,
-})
-
-exports.Order = model('Order', orderSchema);
